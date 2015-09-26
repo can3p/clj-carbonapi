4cad4a1c98f5a1aa3b376a4fccf333170428e16d
Dmitry Petrov <dpetroff@gmail.com>
2015-09-26 21:49:11 +0200

    complete new parsing logic and inject it into existing code

--- src/carbonapi/parser.clj
+++ src/carbonapi/parser.clj
@@ -5,13 +5,15 @@
 (def parser
   (insta/parser
    "source = target | func
-    func = #'[a-zA-Z]+' '(' argument (',' argument)* ')'
+    func = #'[a-zA-Z]+' <'('> argument (<','> argument)* <')'>
     argument = <opt-sp> ( number | string | target | func) <opt-sp>
     string = #'\"[a-zA-Z0-9_-]+\"'
     number = #'\\d+'
     opt-sp = #'[ ]*'
-    target = chunk ( <'.'> target-star* chunk )*
+    target = first-chunk ( <'.'> target-star* chunk )*
     chunk = ( target-literal | target-star | target-enum | target-range )+
+    first-chunk = ( first-target-literal | target-star | target-enum | target-range )+
+    first-target-literal = #'[a-zA-Z][a-zA-Z0-9_]*'
     target-literal = #'[a-zA-Z0-9_]+'
     target-star = '*'
     target-range = <'['> #'[a-zA-Z_-]+' <']'>
@@ -36,21 +38,7 @@
 ;; (decompose-target "abs(avg(test.*, 2), \"abc\")") will result in
 ;; ("abs" ("avg" [:target "test.*"] 2) "abc")
 
-(defn- extract-targets-flat [[h & r]]
-  (cond
-    (= nil h) nil
-    (= h :grep-target) (conj (extract-targets-flat (rest r)) (first r))
-    :else (extract-targets-flat r)))
-
-(defn- extract-targets [tree]
-  (let [f-list (flatten tree)]
-    (extract-targets-flat f-list)))
-
-(defn parse-query [t]
-  (let [tree (decompose-target t)]
-    [(extract-targets tree) tree]))
-
-(defn- build-chunk [els]
+(defn- build-target-chunk [els]
   (let [build (fn [[symb arg :as args]]
                 (case symb
                   :target-literal arg
@@ -65,27 +53,58 @@
 (defn build-target [l]
   (clojure.string/join
    "."
-   (map build-chunk l)))
+   (map build-target-chunk l)))
+
+(defn- build-matcher-chunk [els]
+  (let [build (fn [[symb arg :as args]]
+                (case symb
+                  :target-literal arg
+                  :target-star "[^.]*"
+                  :target-enum (str "("
+                                    (clojure.string/join
+                                     "|"
+                                     (map second (rest args)))
+                                    ")")))]
+    (apply str (map build els))))
+
+(defn build-matcher [l]
+  (re-pattern
+   (clojure.string/join
+    "\\."
+    (map build-matcher-chunk l))))
 
 (defn matches? [pattern val]
   (not (nil? (re-find pattern val))))
 
 (defn- grep-target [targets target]
-  (let [ptrn (re-pattern (clojure.string/replace target "*" "[^.]+"))]
+  (let [ptrn (build-matcher target)]
     (filter (fn [t]
               (matches? ptrn
                         (:target t)))
             targets)))
 
-(defn eval-tree [targets t]
-  (let [eval-map {
-                  :call-func carbonapi.functions/call-func
-                  :grep-target (partial grep-target targets)
-                  }
-        eval-func (fn [form]
+(defn- eval-tree-custom [t eval-map]
+  (let [eval-func (fn [form]
                     (cond
                       (and (seq? form) (contains? eval-map (first form)))
                       (apply (eval-map (first form)) (rest form))
                       :else form
                       ))]
     (clojure.walk/postwalk eval-func t)))
+
+(defn eval-tree [targets t]
+  (let [eval-map {
+                  :call-func carbonapi.functions/call-func
+                  :grep-target (partial grep-target targets)
+                  }]
+    (eval-tree-custom t eval-map)))
+
+(defn- extract-targets [tree]
+  (->> tree
+       (tree-seq seq? identity)
+       (filter #(and (seq? %) (= :grep-target (first %))))
+       (map (comp build-target second))))
+
+(defn parse-query [t]
+  (let [tree (decompose-target t)]
+    [(extract-targets tree) tree]))
--- test/carbonapi/parser-test.clj
+++ test/carbonapi/parser-test.clj
@@ -6,3 +6,22 @@
   (let [target "target.{1,2}_record*.*"]
     (is (= target (build-target (-> target decompose-target second)))
         "parser should be able to construct target back")))
+
+(deftest target-matching
+  (let [target "target.{1,2}_record*.*"
+        matcher (-> target decompose-target second build-matcher)
+        targets [
+                 ["target.1_record.two" true]
+                 ["target.2_recordbla.two" true]
+                 ["target.3_record.two" false]
+                 ["target.2_recordbla" false]
+                 ]]
+    (doseq [[trg should-match?] targets]
+      (is (= (matches? matcher trg) should-match?) (str "matching " trg "should result in " should-match?))
+      )))
+
+(deftest targets-extraction
+  (let [target "abs(target.{1,2}_record*.*,2,test.count)"
+        [targets tree] (parse-query target)
+        targets-set #{"target.{1,2}_record*.*" "test.count"}]
+    (is (= targets-set (set targets)) "parse-query should extract targets from arguments")))
